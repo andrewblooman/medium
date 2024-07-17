109ff578243d63e9b2e9619d35a301f6e0375701
Andrew Blooman <andrew.blooman@gmail.com>
2024-07-17 23:22:14 +0100

    added oidc project

---
+++ oidc_auth_using_jwt/main.tf
@@ -0,0 +1,35 @@
+resource "aws_iam_openid_connect_provider" "gitlab" {
+  url = "https://gitlab.com"
+  client_id_list = [
+    "https://gitlab.com"
+  ]
+  thumbprint_list = ["2B8F1B57330DBBA2D07A6C51F70EE90DDAB9AD8E"]
+}
+
+resource "aws_iam_role" "gitlab_cicd_oidc" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::${var.account_id}:oidc-provider/${var.oidc_provider}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringLike": {
+          "gitlab.com:sub": "project_path:${var.gitlab_project}/*:ref_type:branch:ref:*"
+        }
+      }
+    }
+  ]
+}
+EOF
+
+  name = "gitlab_cicd_oidc"
+  managed_policy_arns = [
+    "arn:aws:iam::696240794375:role/aws-service-role/ssm.amazonaws.com/AWSServiceRoleForAmazonSSM"
+  ]
+}
+
---
+++ oidc_auth_using_jwt/variables.tf
@@ -0,0 +1,16 @@
+variable "account_id" {
+  type    = string
+  default = "ACCOUNT_ID"
+}
+
+
+variable "oidc_provider" {
+  type    = string
+  default = "gitlab.com"
+}
+
+
+variable "gitlab_project" {
+  type    = string
+  default = "PROJECT_PATH"
+}
