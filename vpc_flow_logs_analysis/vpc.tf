c6d841f18b333ba83c65d52afefac06834848280
Andrew Blooman <andrew.blooman@gmail.com>
2024-08-31 19:04:20 +0100

    updated main

--- vpc_flow_logs_analysis/vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_flow_log" "vpc_flow_log" {
-  iam_role_arn         = aws_iam_role.vpc_flow_log_role.arn
-  log_destination      = aws_cloudwatch_log_group.vpc_logs_group.arn
-  log_destination_type = "cloud-watch-logs"
-  traffic_type         = "ALL"
-  vpc_id               = data.aws_vpc.main.id
-}
